f6074112840ad263ef3407098b11e4162b0018e5
Małgorzata Stolarczyk <gosia.stolarczyk@gmail.com>
2023-02-13 19:18:10 +0100

    intro

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {         // [2]
+        System.out.println("Hello World!");
+        System.out.println("Małgorzata");
+        System.out.println("40+");
+        System.out.println("S");// [3]
+    }
+}
