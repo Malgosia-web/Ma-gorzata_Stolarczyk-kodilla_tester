ab72039fcf53234bc17bc686c93fca57c7a96d53
Małgorzata Stolarczyk <gosia.stolarczyk@gmail.com>
2023-03-26 11:33:37 +0200

    Add MainExtended

---
+++ kodilla-intro/src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String myName = "Gosia";
+        int myAge = 22;
+        double height = 1.78;
+        char carClass = 'A';
+        boolean isGoodProgrammer = true;
+        System.out.println(myName);
+        System.out.println(carClass);
+    }
+}
---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,18 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String flowerName = "Zdechlak";
+        int greenLeaves = 29;
+        int yellowLeaves = 32;
+        int age = 11;
+        double score = 83.5;
+        char grade = 'D';
+        boolean flowerIsAlive = true;
+        System.out.println(flowerName);
+        System.out.println(greenLeaves);
+        System.out.println(yellowLeaves);
+        System.out.println(age);
+        System.out.println(score);
+        System.out.println(grade);
+        System.out.println(flowerIsAlive);
+    }
+}
