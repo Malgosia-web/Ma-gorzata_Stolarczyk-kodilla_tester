d842e7ff323d964ded1008903f9999bc21966a03
Małgorzata Stolarczyk <gosia.stolarczyk@gmail.com>
2023-04-18 16:33:34 +0200

    Zadanie 2.5 pętle

---
+++ kodilla-intro/src/main/java/RandomNumbers.java
@@ -0,0 +1,11 @@
+/*public class RandomNumbers {
+    public static void main(String[] args) {
+        RandomNumbers liczba = new RandomNumbers();
+        RandomNumbers generator = new RandomNumbers();
+
+        for (int i = 0; i < 36; i++) {
+
+            liczba.add(generator.nextInt(50));
+        }
+    }
+}*/
