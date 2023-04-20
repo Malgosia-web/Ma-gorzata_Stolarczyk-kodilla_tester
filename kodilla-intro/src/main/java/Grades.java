2a95bd54a8f0b87bbbe8d9fe5b4afe8804cd84ed
Małgorzata Stolarczyk <gosia.stolarczyk@gmail.com>
2023-04-20 09:36:14 +0200

    estetyka

--- kodilla-intro/src/main/java/Grades.java
+++ kodilla-intro/src/main/java/Grades.java
@@ -14,27 +14,19 @@ public class Grades {                 //tworzymy klase
         }
         this.grades[this.size] = value;  //jeśli jest miejsce to dodajemy element o konkretnym indeksie równym obecnemu rozmiarowi
         this.size++;   ///ilość elementów w tablicy zostaje zwiększona
-        System.out.println("dodana ocena to: " + value);
     }
 
     //METODA ZWRACAJĄCA OSTATNIĄ OCENĘ
     int showLast() {
         int lastOne = grades[(this.size - 1)];
-        System.out.println("ostatnia ocena to: " + lastOne);
         return lastOne;
-        //System.out.println("this.size to: " + this.size);
-        //System.out.println("grades[this.size - 1] to: " + grades[this.size -1]);
     }
-
-    //METODA ZWRACAJĄCA ŚREDNIĄ - nie wiem jak zrobić, żeby było po przecinku, zawsze zaokrągla do całości
+    //METODA WYLICZAJĄCA ŚREDNIĄ OCEN
     double checkAverage() {
-        //double average = (grades[0] + grades[1] + grades[2] + grades[3] + grades[4] + grades[5] + grades[6] + grades[7] + grades[8] + grades[9]) / (this.size);
-        //System.out.println("średnia ocen zaokrąglona do całości to: " + average);
         double sum = 0;
         for (int i = 0; i < 10; i++) {
             sum += grades[i];
         }
-        System.out.println("średnia ocen to: " + sum / size);
         return sum / size;
     }
 
@@ -42,13 +34,11 @@ public class Grades {                 //tworzymy klase
         Grades grades = new Grades();
         grades.add(3);
         grades.add(6);
-        grades.checkAverage();
         grades.add(1);
-        grades.showLast();
         grades.add(4);
         grades.add(3);
-        grades.checkAverage();
-        grades.showLast();
 
+        System.out.println("ostatnia dodana ocena to: " + grades.showLast());
+        System.out.println("średnia wszystkich ocen to: " + grades.checkAverage());
     }
 }
