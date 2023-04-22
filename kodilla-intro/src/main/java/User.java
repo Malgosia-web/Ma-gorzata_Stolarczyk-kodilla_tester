6ef370157d9a32bbfc759f1fb283fc11854608ca
Małgorzata Stolarczyk <gosia.stolarczyk@gmail.com>
2023-04-22 14:44:14 +0200

    zadanie 2.5. pętle kolejne

--- kodilla-intro/src/main/java/User.java
+++ kodilla-intro/src/main/java/User.java
@@ -1,42 +1,34 @@
 public class User {
-        private String[] user;
-        private String name;
-        private int age;
-        private int average;
+    private String name;
+    private int age;
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public static void main(String[] args) {
+        User a = new User("Stefan", 13);
+        User b = new User("Ania", 33);
+        User c = new User("Adam", 53);
+        User d = new User("Zbyszek", 3);
+        User e = new User("Basia", 23);
 
-        public User(String name, int age) {
-            this.user = new String[5];
-            this.name = name;
-            this.age = age;
+        User[] users = {a, b, c, d, e};
+
+        System.out.println("użytkownicy: " + users.length);
+
+        // wyliczanie średniej wieku:
+        double sum = 0;
+        for (int i = 0; i < users.length; i++) {
+            sum += users[i].age;
         }
+        double average = sum / users.length;
+        System.out.println("średnia wieku wszystkich użytkowników to: " + average);
 
-        public void checkAge(){
-            for (int i = 0; i < average; i++) {
-                if (this.age == i) {    //nie mam pojęcia jak zrobić zbiorczo
-                    System.out.println("Wiek poniżej średniej ma: " + this.name);
-                }
+        //wiek poniżej średniej ma:
+        for (int i = 0; i < users.length; i++) {
+            if (users[i].age < average) {
+                System.out.println("Wiek poniżej średniej ma: " + users[i].name);
             }
         }
-
-        public static void main(String[] args) {
-            User a = new User("Stefan", 13);
-            User b = new User("Ania", 33);
-            User c = new User("Adam", 53);
-            User d = new User("Zbyszek", 3);
-            User e = new User("Basia", 23);
-
-            User[] users = {a, b, c, d, e};
-            System.out.println("użytkownicy: " + users.length);
-            int average = (a.age + b.age + c.age + d.age + e.age) / users.length; //wylicza średnią
-            System.out.println("średnia wieku: " + average);
-            //System.out.println("a.age: " + a.age);
-            //System.out.println("a.name: " + a.name);
-
-            //wiek poniżej średniej ma:
-            for (int i = 0; i < average; i++) {
-                if (a.age == i) {    //nie mam pojęcia jak zrobić zbiorczo
-                    System.out.println("Wiek poniżej średniej ma: " + a.name);
-                }
-            }
-        }
+    }
 }
