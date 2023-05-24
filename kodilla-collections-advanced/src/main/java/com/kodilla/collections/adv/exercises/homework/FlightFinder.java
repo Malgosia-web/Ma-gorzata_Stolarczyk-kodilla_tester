5e823b825a8d231517c5044a72a0e28db7a38f8b
Małgorzata Stolarczyk <gosia.stolarczyk@gmail.com>
2023-05-24 15:14:29 +0200

    zadanie 5.4 "lista lotów" - poprawione

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/exercises/homework/FlightFinder.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/exercises/homework/FlightFinder.java
@@ -5,15 +5,11 @@ import java.util.List;
 
 public class FlightFinder {
 
-    private String departure;
-    private String arrival;
-
     public List<Flight> findFlightsFrom(String departure) {
-        this.departure = departure;
         List<Flight> from = new ArrayList<>();
 
         for (Flight f : FlightRepository.getFlightTable()) {
-            if (f.getDeparture() == this.departure) {
+            if (f.getDeparture().equals(departure)) {
                  from.add(f);
             }
         }
@@ -21,11 +17,10 @@ public class FlightFinder {
     }
 
     public List<Flight> findFlightsTo(String arrival) {
-        this.arrival = arrival;
         List<Flight> to = new ArrayList<>();
 
         for (Flight a : FlightRepository.getFlightTable()) {
-            if (a.getArrival() == this.arrival) {
+            if (a.getArrival().equals(arrival)) {
                 to.add(a);
             }
         }
