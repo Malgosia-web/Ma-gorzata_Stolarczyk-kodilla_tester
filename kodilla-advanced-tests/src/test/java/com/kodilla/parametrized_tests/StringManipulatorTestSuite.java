243402c0f22665311f966a3d8f1c1aa1330a84b4
Małgorzata Stolarczyk <gosia.stolarczyk@gmail.com>
2023-06-13 16:21:50 +0200

    przykłady 7

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/StringManipulatorTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/StringManipulatorTestSuite.java
@@ -1,20 +1,36 @@
 package com.kodilla.parametrized_tests;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringManipulatorTestSuite {
     private StringManipulator manipulator = new StringManipulator();
+    private static Stream<Arguments> provideStringsForTestingLength() {
+        return Stream.of(
+                Arguments.of("test", 4),
+                Arguments.of("OtHEr ", 5),
+                Arguments.of("E V e n t", 5),
+                Arguments.of("null ", 4),
+                Arguments.of("A", 1)
+        );
+    }
 
     @ParameterizedTest
-    @CsvSource(value = {"test,tset", "OtHEr,rehto", "EVent,tneve"})
+    @CsvFileSource(resources = "/stringWithLowerCase.csv", numLinesToSkip = 1)
+    //@CsvSource(value = {"test,tset", "OtHEr,rehto", "EVent,tneve", "null,llun", "A,a"})
     public void shouldReverseStringWithLowerCase(String input, String expected) {
         assertEquals(expected, manipulator.reverseWithLowerCase(input));
     }
     @ParameterizedTest
-    @CsvSource(value = {"test,4", " OtHEr ,5", "E V e n t,5", "null ,4"})
+    @MethodSource(value = "com.kodilla.parametrized_tests.StringSource#provideStringsForTestingLength")
+    //@MethodSource(value = "provideStringsForTestingLength")
     public void shouldCalculateStringLengthWithoutSpaces(String input, int expected) {
         assertEquals(expected, manipulator.getStringLengthWithoutSpaces(input));
     }
