6e377f636e84c16f5068ee779ea0d37a00e0cedf
Małgorzata Stolarczyk <gosia.stolarczyk@gmail.com>
2023-06-19 09:29:27 +0200

    przykład 7.5 Mockito poprawione

--- kodilla-advanced-tests/src/main/java/com/kodilla/mockito/homework/WeatherService.java
+++ kodilla-advanced-tests/src/main/java/com/kodilla/mockito/homework/WeatherService.java
@@ -1,40 +1,53 @@
 package com.kodilla.mockito.homework;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class WeatherService {
-    private Map<WeatherClient, Area> clients = new HashMap<>();
+    private Set<WeatherClient> area1 = new HashSet<>();
+    private Set<WeatherClient> area2 = new HashSet<>();
+    private Set<WeatherClient> area3 = new HashSet<>();
+    Set<WeatherClient> global = new HashSet<>();
+
+    //dodaje chętnych
+    public void addSubscriber(WeatherClient client, int area) {
+        if (area == 1)  {this.area1.add(client); this.global.add(client);}
+        else if (area == 2) {this.area2.add(client); this.global.add(client);}
+        else if (area == 3) {this.area3.add(client); this.global.add(client);}
+        else System.out.println("nie ma takiego obszaru");
+    }
 
     //wysyła alert do wszystkich, niezależnie od rejonu
     public void sendGlobalAlert(WeatherAlert alert) {
-        for(Map.Entry<WeatherClient, Area> global : clients.entrySet()) {
-            global.getKey().receive(alert);
-        }
+        this.global.forEach(client -> client.receive(alert));
     }
+
     //wysyła alert do zapisanych na konkretny rejon
-    public void sendAreaAlert(WeatherAlert alert, Area area) {
-        for(Map.Entry<WeatherClient, Area> local : clients.entrySet()) {
-            if (local.getValue() == area) {
-                local.getKey().receive(alert);
-            }
-        }
+    public void sendAreaAlert(WeatherAlert alert, int area) {
+        if (area == 1) this.area1.forEach(client -> client.receive(alert));
+        else if (area == 2) this.area2.forEach(client -> client.receive(alert));
+        else if (area == 3) this.area3.forEach(client -> client.receive(alert));
+        else System.out.println("nie ma takiego obszaru");
     }
-    //dodaje chętnych
-    public void addSubscriber(WeatherClient client, Area area) {
-        this.clients.put(client, area);
-    }
+
     //usuwa z wszystkich alertów
     public void removeSubscriberGlobal(WeatherClient client) {
-        this.clients.remove(client);
+        this.area1.remove(client);
+        this.area2.remove(client);
+        this.area3.remove(client);
+        this.global.remove(client);
     }
     //usuwa z lokalnych alertów
-    public void removeSubscriberFromArea(WeatherClient client, Area area) {
-        this.clients.remove(client, area);
+    public void removeSubscriberFromArea(WeatherClient client, int area) {
+        if (area == 1) this.area1.remove(client);
+        else if (area == 2) this.area2.remove(client);
+        else if (area == 3) this.area3.remove(client);
+        else System.out.println("nie ma takiego obszaru");
     }
     //usuwa rejon
-    public void removeArea(Area area) {
-        this.clients.values().removeAll(Collections.singleton(area));
+    public void removeArea(int area) {
+        if (area == 1) this.area1.clear();
+        else if (area == 2) this.area2.clear();
+        else if (area == 3) this.area3.clear();
+        else System.out.println("nie ma takiego obszaru");
     }
 }
--- kodilla-advanced-tests/src/test/java/com/kodilla/mockito/homework/WeatherServiceTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/mockito/homework/WeatherServiceTestSuite.java
@@ -12,63 +12,70 @@ class WeatherServiceTestSuite {
     WeatherClient client3 = Mockito.mock(WeatherClient.class);
     WeatherClient client4 = Mockito.mock(WeatherClient.class);
 
-    Area a1 = Mockito.mock(Area.class);
-    Area a2 = Mockito.mock(Area.class);
-
     WeatherAlert globalAlert = Mockito.mock(WeatherAlert.class);
     WeatherAlert areaAlert = Mockito.mock(WeatherAlert.class);
 
     @BeforeEach
     public void add() {
-        weatherService.addSubscriber(client1, a1);
-        weatherService.addSubscriber(client2, a1);
-        weatherService.addSubscriber(client3, a2);
+        weatherService.addSubscriber(client1, 1);
+        weatherService.addSubscriber(client1, 2);
+        weatherService.addSubscriber(client2, 2);
+        weatherService.addSubscriber(client3, 1);
+        weatherService.addSubscriber(client3, 2);
+        weatherService.addSubscriber(client3, 3);
     }
 
     //client4 nie jest nigdzie zapisany, nie powinien nic dostać
-    @Test //OK
+    @Test
     public void notSubscribedClientShouldNotReceiveAlert() {
         weatherService.sendGlobalAlert(globalAlert);
         Mockito.verify(client4, Mockito.never()).receive(globalAlert);
     }
-    //client1 wypisuje się z a1
-    @Test
-    public void unsubscribedClientShouldNotReceiveAreaAlert() {
-        weatherService.removeSubscriberFromArea(client1, a1);
-        weatherService.sendAreaAlert(areaAlert, a1);
-        Mockito.verify(client1, Mockito.never()).receive(areaAlert);
-        Mockito.verify(client2).receive(areaAlert);
-    }
-    //client1 wypisuje się globalnie
-    @Test //OK
-    public void unsubscribedClientShouldNotReceiveAlert() {
-        weatherService.removeSubscriberGlobal(client1);
-        weatherService.sendGlobalAlert(globalAlert);
-        Mockito.verify(client1, Mockito.never()).receive(globalAlert);
-        Mockito.verify(client2).receive(globalAlert);
-    }
+
     //każdy zapisany dostaje alert globalny
-    @Test //OK
+    @Test
     public void subscribedClientShouldReceiveAlert() {
         weatherService.sendGlobalAlert(globalAlert);
         Mockito.verify(client1).receive(globalAlert);
         Mockito.verify(client2).receive(globalAlert);
         Mockito.verify(client3).receive(globalAlert);
     }
-    //client1 i client2 dostają lokalny alert a1
-    @Test //OK
+
+    //client1 i client3 dostają lokalny alert area1, client2 nie
+    @Test
     public void subscribedClientShouldReceiveAreaAlert() {
-        weatherService.sendAreaAlert(areaAlert, a1);
+        weatherService.sendAreaAlert(areaAlert, 1);
         Mockito.verify(client1).receive(areaAlert);
-        Mockito.verify(client2).receive(areaAlert);
-        Mockito.verify(client3, Mockito.never()).receive(areaAlert);
+        Mockito.verify(client3).receive(areaAlert);
+        Mockito.verify(client2, Mockito.never()).receive(areaAlert);
     }
-    //client1 i client2 nie dostają alertów z usuniętego a1
+
+    //client1 wypisuje się z area1, client3 zostaje
+    @Test
+    public void unsubscribedClientShouldNotReceiveAreaAlert() {
+        weatherService.removeSubscriberFromArea(client1, 1);
+        weatherService.sendAreaAlert(areaAlert, 1);
+        Mockito.verify(client1, Mockito.never()).receive(areaAlert);
+        Mockito.verify(client3).receive(areaAlert);
+    }
+
+    //client1 wypisuje się globalnie
+    @Test
+    public void unsubscribedClientShouldNotReceiveAlert() {
+        weatherService.removeSubscriberGlobal(client1);
+        weatherService.sendGlobalAlert(globalAlert);
+        weatherService.sendAreaAlert(areaAlert, 1);
+        weatherService.sendAreaAlert(areaAlert, 2);
+        Mockito.verify(client1, Mockito.never()).receive(globalAlert);
+        Mockito.verify(client1, Mockito.never()).receive(areaAlert);
+    }
+
+    //client1 i client3 nie dostają alertów z usuniętego area1
     @Test
     public void deleteArea() {
-        weatherService.removeArea(a1);
-        weatherService.sendAreaAlert(areaAlert, a1);
+        weatherService.removeArea(1);
+        weatherService.sendAreaAlert(areaAlert, 1);
         Mockito.verify(client1, Mockito.never()).receive(areaAlert);
-        Mockito.verify(client2, Mockito.never()).receive(areaAlert);
+        Mockito.verify(client3, Mockito.never()).receive(areaAlert);
     }
 }
