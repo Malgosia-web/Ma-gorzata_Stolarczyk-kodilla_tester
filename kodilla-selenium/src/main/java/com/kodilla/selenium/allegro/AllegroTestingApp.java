780d4773ad1609a75b44439edc64b6690bb3e673
Małgorzata Stolarczyk <gosia.stolarczyk@gmail.com>
2023-07-12 11:20:29 +0200

    zadanie 13.5 allegro

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -5,7 +5,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
 
 public class AllegroTestingApp {
     public static void main(String[] args) {
@@ -13,16 +18,22 @@ public class AllegroTestingApp {
         WebDriver driver = new ChromeDriver();
         driver.get("https://allegro.pl");
 
-        WebElement cookiesField = driver.findElement(By.xpath("//*/button[1]"));
+        WebElement cookiesField = driver.findElement(By.cssSelector("div button"));
         cookiesField.click();
 
-        WebElement selectField = driver.findElement(By.xpath("//*/select[1]"));
+        WebElement selectField = driver.findElement(By.cssSelector("div select"));
         Select categorySelect = new Select(selectField);
         categorySelect.selectByValue("/kategoria/elektronika");
 
-        WebElement inputField = driver.findElement(By.xpath("//*/input[1]"));
+        WebElement inputField = driver.findElement(By.cssSelector("div input"));
         inputField.sendKeys("Mavic mini");
 
         inputField.submit();
+
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.cssSelector("div article h2"))));
+
+        List<WebElement> element = driver.findElements(By.cssSelector("div article h2"));
+        System.out.println(element.get(0).getText());
     }
 }
