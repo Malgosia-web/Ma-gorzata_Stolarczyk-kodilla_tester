b7e89406d98b585d3cea07ce7c88691e373e5369
Małgorzata Stolarczyk <gosia.stolarczyk@gmail.com>
2023-07-21 08:46:34 +0200

    przykład 14.2 Cucumber

---
+++ kodilla-cucumber/src/test/java/seasonchecker/IsItSummerSteps.java
@@ -0,0 +1,43 @@
+package seasonchecker;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import java.time.MonthDay;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seasonchecker.SeasonChecker.checkIfSummer;
+
+public class IsItSummerSteps {
+    private MonthDay today;
+    private String answer;
+    @Given("today is last day of December")
+    public void today_is_last_day_of_december() {
+        this.today = MonthDay.of(12, 31);
+        /*
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+        */
+    }
+    @Given("today is first day of August")
+    public void today_is_first_day_of_august() {
+        this.today = MonthDay.of(8, 1);
+    }
+    @When("I ask whether it's Summer")
+    public void i_ask_whether_it_s_summer() {
+        this.answer = checkIfSummer(this.today);
+        /*
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+         */
+    }
+    @Then("I should be told {string}")
+    public void i_should_be_told(String string) {
+        assertEquals(string, this.answer);
+        /*
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+         */
+    }
+}
